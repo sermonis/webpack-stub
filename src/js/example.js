fed0e499c86feee2e5e03b5366b70b99325035e7
woodell <dev@sermonis.ru>
2021-01-26 17:41:21 +0300

    initial upload

---
+++ src/js/example.js
@@ -0,0 +1,8 @@
+const example = () => {
+	return (
+		'Basic Webpack 5 project stub with a hot reload ' +
+		'development server and an optimized production build.'
+	);
+};
+
+export { example as default };
